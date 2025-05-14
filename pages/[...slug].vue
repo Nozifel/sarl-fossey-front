09c74c312e77219835eeb068d608d4a9ba727a31
Alexy Haluch <a.haluch@cicd.biz>
2025-05-14 16:10:52 +0200

    tpl

---
+++ pages/[...slug].vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { data: page } = await useAsyncData(() => queryCollection("content").path("/").first());
+
+useSeoMeta({
+	title: page.value?.title,
+	description: page.value?.description,
+});
+</script>
+
+<template>
+	<template v-if="page">
+		<ContentRenderer :value="page" />
+	</template>
+	<template v-else>
+		<div class="empty-page">
+			<h1>Page Not Found</h1>
+			<p>Oops! The content you're looking for doesn't exist.</p>
+			<NuxtLink to="/">Go back home</NuxtLink>
+		</div>
+	</template>
+</template>
